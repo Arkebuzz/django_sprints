6b09cc347ec5d688d60f3cf4659463651933fbdb
Arkebuzz <aykoret@gmail.com>
2025-05-14 22:29:17 +0300

    Configure the admin panel

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -2,6 +2,77 @@ from django.contrib import admin
 
 from .models import Category, Location, Post
 
-admin.site.register(Category)
-admin.site.register(Location)
-admin.site.register(Post)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'slug',
+        'is_published',
+        'description',
+        'created_at',
+    )
+    list_editable = (
+        'slug',
+        'is_published',
+    )
+    search_fields = (
+        'title',
+        'slug',
+    )
+    list_filter = (
+        'is_published',
+    )
+
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'is_published',
+        'created_at',
+    )
+    list_editable = (
+        'is_published',
+    )
+    search_fields = (
+        'name',
+    )
+    list_filter = (
+        'is_published',
+    )
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'author',
+        'location',
+        'category',
+        'text',
+        'is_published',
+        'pub_date',
+        'created_at',
+    )
+    list_editable = (
+        'is_published',
+    )
+    search_fields = (
+        'title',
+        'author',
+        'location',
+        'category',
+    )
+    list_filter = (
+        'is_published',
+        'location',
+        'category',
+    )
+    list_display_links = (
+        'author',
+        'location',
+        'category',
+    )
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Post, PostAdmin)
